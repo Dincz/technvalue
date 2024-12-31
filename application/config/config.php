ff01ad57ffb09b361e6e3bdf637fbdc7cf759c10
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-12-31 11:52:34 +0530

    pulling new code for data fetching by seo

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/technvalue/';
+$config['base_url'] = 'http://localhost/technovalue/';
 // $config['base_url'] = 'https://labindiadigital.com/technovalue/';
 
 /*
